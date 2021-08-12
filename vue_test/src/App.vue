acf60603f1cd3d2fb684ed7acdfabd311ba58050
Yu <leoleo5994@gmail.com>
2021-08-12 18:40:48 +0800

    開始學習Vue CLI

---
+++ vue_test/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Student />
+    <School />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import Student from "./components/Student.vue";
+import School from "./components/School.vue";
+
+export default {
+  name: "App",
+  components: {
+    Student,
+    School,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue_test/src/components/Student.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <h1>{{ sex }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Yu",
+      sex: "男性",
+    };
+  },
+};
+</script>
+
+<style></style>
