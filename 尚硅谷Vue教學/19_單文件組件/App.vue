c31568c5b7b2f4a4b735e8d633b116e722581cc0
Yu <leoleo5994@gmail.com>
2021-08-12 18:42:28 +0800

    建立vue_test專案練習CLI

---
+++ 尚硅谷Vue教學/19_單文件組件/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+import Student from "./Student.vue";
+import School from "./Student.vue";
+
+export default {
+  components: {
+    Student,
+    School,
+  },
+};
+</script>
+
+<style></style>
---
+++ 尚硅谷Vue教學/19_單文件組件/main.js
@@ -0,0 +1,11 @@
+import App from "./App.vue";
+
+const vm = new Vue({
+  el: "#root",
+  data() {
+    return {};
+  },
+  componenets: {
+    App,
+  },
+});
