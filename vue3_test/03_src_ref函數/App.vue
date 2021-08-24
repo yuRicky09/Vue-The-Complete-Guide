d88db38685f84bab548f063020bb02f3196dced6
Yu Ricky <leoleo5994@gmail.com>
2021-08-24 13:10:13 +0800

    學習Vue2與Vue3響應式原理的差異

---
+++ vue3_test/03_src_ref函數/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <h1>{{ age }}</h1>
+    <br />
+    <button @click="showAlert">點我彈窗</button>
+    <button @click="changeInfo">點我更改資料</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "App",
+  //* Vue3中新的配置項，data method直接寫在裡面
+  setup() {
+    // 使用ref函數定義一個響應式數據
+    // 數據
+    let name = ref("Yu_Ricky");
+    let age = ref(18);
+    let person = ref({
+      name: "Yu",
+      age: 18,
+      sex: "男",
+    });
+
+    // 方法
+    const showAlert = function() {
+      alert("我跳出來拉");
+    };
+
+    const changeInfo = function() {
+      name.value = "Lebron";
+      age.value = 36;
+      console.log(peorson.value);
+    };
+
+    //* setup函數的返回值
+    //! 1. obj(常用)
+    return {
+      name,
+      age,
+      showAlert,
+      changeInfo,
+      person,
+    };
+
+    //! 2. 返回一個渲染函數 (不太會用到)
+    // return () => h("h1", "我是渲染函數的返回內容");
+  },
+};
+</script>
+
+<style></style>
--- vue3_test/src/App.vue
+++ vue3_test/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1>{{ name }}</h1>
-    <h1>{{ age }}</h1>
+    <h1>{{ person.name }}</h1>
+    <h1>{{ person.teamNumber.laker }}</h1>
     <br />
     <button @click="showAlert">點我彈窗</button>
     <button @click="changeInfo">點我更改資料</button>
@@ -9,20 +9,21 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive } from "vue";
 
 export default {
   name: "App",
-  //* Vue3中新的配置項，data method直接寫在裡面
   setup() {
-    // 使用ref函數定義一個響應式數據
     // 數據
-    let name = ref("Yu_Ricky");
-    let age = ref(18);
-    let person = ref({
-      name: "Yu",
-      age: 18,
+    const person = reactive({
+      name: "LeBron",
       sex: "男",
+      position: ["SF", "PG", "PF", "C"],
+      teamNumber: {
+        laker: 23,
+        heat: 6,
+        cavs: 23,
+      },
     });
 
     // 方法
@@ -31,23 +32,15 @@ export default {
     };
 
     const changeInfo = function() {
-      name.value = "Lebron";
-      age.value = 36;
-      console.log(peorson.value);
+      person.name = "LeBron James";
+      person.teamNumber.laker = 6;
     };
 
-    //* setup函數的返回值
-    //! 1. obj(常用)
     return {
-      name,
-      age,
       showAlert,
       changeInfo,
       person,
     };
-
-    //! 2. 返回一個渲染函數 (不太會用到)
-    // return () => h("h1", "我是渲染函數的返回內容");
   },
 };
 </script>
